a8a4d6d06b4e3ff63ecc900943c2645afda6e2a9
Tim Horton <hortont424@gmail.com>
2011-09-25 16:44:36 -0700

    you can make filters now!!!

--- scripts/library.js
+++ scripts/library.js
@@ -1,9 +1,15 @@
+// weekend project, so it's pretty terrible!
+
 var liveNodes = [];
 var outgoingNoodles = {}; // filterBubble[id] -> noodle-element
 var noodleConnections = {}; // filterBubble[id] -> filterBubble[id]
-var parameterMap = {}; //filterBubble[id] -> [filterNode[id], parameter]
-var currentID = 0;
-var masterInput, masterOutput;
+var parameterMap = {}; // filterBubble[id] -> [filterNode[id], parameter]
+var reverseParameterMap = {}; // filterNode[id] -> {parameter: filterBubble[id]}
+var currentID = 0, currentResultID = 0;
+var masterInput;
+var filterTypes = {}; // filterNode[id] -> filter object
+
+var SVGNS = "http://www.w3.org/2000/svg";
 
 function removeNoodle(id)
 {
@@ -31,6 +37,12 @@ function newID()
     return "id" + currentID;
 }
 
+function newResultID()
+{
+    currentResultID++;
+    return "result" + currentResultID;
+}
+
 function connectNoodles(output, input)
 {
     for(id in noodleConnections)
@@ -50,26 +62,129 @@ function connectNoodles(output, input)
     return true;
 }
 
-function updateFilter()
+function getConnectedElements(startId)
 {
-    var filterString = "";
-    var nextFEId = masterInput.attr("id");
-
-    for(fe in noodleConnections)
+    function getChildren(id)
     {
-        parameterFrom = parameterMap[fe];
-        parameterTo = parameterMap[noodleConnections[fe]];
-        filterString += parameterFrom[0] + "(" + parameterFrom[1] + ")" + " -> " + parameterTo[0] + "(" + parameterTo[1] + ")" + "; ";
+        var children = [];
+
+        for(paramName in reverseParameterMap[id])
+        {
+            var ch = reverseParameterMap[id][paramName];
+
+            if(ch in noodleConnections)
+            {
+                children.push(noodleConnections[ch]);
+                children.push(parameterMap[noodleConnections[ch]][0]);
+            }
+
+            children.push(ch);
+        }
+
+        return children;
     }
 
-    $("#previewContainer").html(filterString);
+    var connectedElements = {startId: 1};
+    var searchStack = [startId];
+
+    while(searchStack.length)
+    {
+        var cid = searchStack.pop();
+        var children = getChildren(cid);
+
+        for(child in children)
+        {
+            var childId = children[child];
+            if(!(childId in connectedElements))
+            {
+                connectedElements[childId] = 1;
+                searchStack.push(childId);
+            }
+        }
+    }
+
+    return connectedElements;
+}
+
+function updateFilter()
+{
+    var filter = document.createElementNS(SVGNS, "filter");
+    var resultIDForOutputBubbleId = {};
+    var filterElementForFilterId = {};
+
+    currentResultID = 0;
+
+    filter.setAttribute("id", "filt");
+
+    var reachableFilterElements = getConnectedElements(masterInput.attr("id"));
+
+    for(filterId in filterTypes)
+    {
+        if(!(filterId in reachableFilterElements))
+            continue;
+
+        var filterElement = document.createElementNS(SVGNS, filterTypes[filterId]["displayName"]);
+        filterElementForFilterId[filterId] = filterElement;
+        var resultId = newResultID();
+        resultIDForOutputBubbleId[reverseParameterMap[filterId]["result"]] = resultId;
+        filterElement.setAttribute("result", resultId);
+        filter.appendChild(filterElement);
+    }
+
+    for(filterId in filterTypes)
+    {
+        if(!(filterId in reachableFilterElements))
+            continue;
+
+        var filterElement = filterElementForFilterId[filterId];
+
+        for(attrName in reverseParameterMap[filterId])
+        {
+            if(attrName == "result")
+                continue;
+
+            var attrBubbleId = reverseParameterMap[filterId][attrName];
+
+            // this is bad!
+            for(fromBubbleId in noodleConnections)
+            {
+                var toBubbleId = noodleConnections[fromBubbleId];
+                if(toBubbleId == attrBubbleId)
+                {
+                    var resultID = resultIDForOutputBubbleId[fromBubbleId];
+                    if(resultID)
+                        filterElement.setAttribute(attrName, resultID);
+                }
+            }
+        }
+
+        for(paramName in filterTypes[filterId]["parameters"])
+        {
+            var paramDetails = filterTypes[filterId]["parameters"][paramName];
+
+            filterElement.setAttribute(paramName, paramDetails[3]);
+        }
+    }
+
+    if(filter.childNodes.length == 0)
+    {
+        // passthrough filter
+        var filterElement = document.createElementNS(SVGNS, "feOffset");
+        filter.appendChild(filterElement);
+    }
+
+    $("#defs").empty();
+    $("#defs").append(filter);
+
+    //$("#previewContainer").html((new XMLSerializer()).serializeToString(filter).replace(/</g, "&lt;").replace(/>/g, "&gt;"));
 }
 
 function updateNoodles()
 {
+    var editorOffset = $("#editor").offset();
+
     for(id in outgoingNoodles)
     {
-        var editorOffset = $("#editor").offset();
         var noodleOffset = $("#" + id).offset();
         var destinationNoodleOffset = $("#" + noodleConnections[id]).offset();
 
@@ -80,7 +195,7 @@ function updateNoodles()
             left: noodleOffset.left - editorOffset.left + 6 + "px",
             top: noodleOffset.top - editorOffset.top + 6 + "px",
 
-            height: Math.sqrt(deltaY * deltaY + deltaX * deltaX),
+            height: Math.round(Math.sqrt(deltaY * deltaY + deltaX * deltaX)),
             webkitTransform: "rotate(" + (Math.atan2(deltaY, deltaX) - Math.PI/2) + "rad)"
         });
     }
@@ -101,27 +216,9 @@ function setupMasterInput()
     masterInput.addClass("feMasterInput");
 
     parameterMap[output.attr("id")] = [masterInput.attr("id"), "result"];
+    reverseParameterMap[masterInput.attr("id")] = {"result": output.attr("id")};
 
     $("#editor").append(masterInput);
-}
-
-function setupMasterOutput()
-{
-    masterOutput = $("<div class='filterNode'><div class='dragHandle'>Filter Output</div></div>");
-    masterOutput.attr("id", newID());
-
-    var output = $("<div style='top: -8px;' class='filterBubble filterBubbleIn' />").appendTo(masterOutput);
-    connectionDrag(output);
-
-    masterOutput.css({bottom: "20px", left: "20px", position: "absolute"});
-
-    editorFilterDrag(masterOutput);
-    masterOutput.addClass("filterNodeLanded");
-    masterOutput.addClass("feMasterOutput");
-
-    parameterMap[output.attr("id")] = [masterOutput.attr("id"), "in"];
-
-    $("#editor").append(masterOutput);
 }
 
 function connectionDrag(el)
@@ -213,6 +310,9 @@ function loadFilters(filters)
         var proxy = $("<div class='filterNode'><div class='dragHandle'>" + f["name"] + "</div></div>");
         proxy.attr("id", newID());
 
+        filterTypes[proxy.attr("id")] = f;
+        reverseParameterMap[proxy.attr("id")] = {};
+
         // eventually these need to be dynamic based on the filter
 
         var inputRight = 8;
@@ -221,12 +321,16 @@ function loadFilters(filters)
             var input = $("<div class='filterBubble filterBubbleIn' />").appendTo(proxy).css({"right": inputRight, "top": -8});
             connectionDrag(input);
             inputRight += 24;
-            parameterMap[input.attr("id")] = [proxy.attr("id"), f["inputs"][i]];
+            var inputAttrName = f["inputs"][i];
+            parameterMap[input.attr("id")] = [proxy.attr("id"), inputAttrName];
+
+            reverseParameterMap[proxy.attr("id")][inputAttrName] = input.attr("id");
         }
 
         var output = $("<div style='bottom: -8px;' class='filterBubble filterBubbleOut' />").appendTo(proxy);
         connectionDrag(output);
         parameterMap[output.attr("id")] = [proxy.attr("id"), "result"];
+        reverseParameterMap[proxy.attr("id")]["result"] = output.attr("id");
 
         return proxy;
     }
@@ -254,6 +358,7 @@ function loadFilters(filters)
             $(dd.proxy).addClass("filterNodeLanded");
             $("#dragMessage").addClass("dragMessageClosed");
             $("#libraryList").addClass("noMessage");
+            updateFilter();
         });
     }
 
